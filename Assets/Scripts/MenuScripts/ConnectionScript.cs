bde7f8c2314fb0e60f2ce4ff89f629b47d9aa470
ciaranmh <stemcan7@gmail.com>
2021-07-03 18:20:56 +0100

    refactored some menu code to be cleaner

--- Assets/Scripts/MenuScripts/ConnectionScript.cs
+++ Assets/Scripts/MenuScripts/ConnectionScript.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using MLAPI;
+using MLAPI.Messaging;
 using MLAPI.Spawning;
 using UnityEngine;
 
@@ -10,13 +12,14 @@ namespace MenuScripts
     {
         public GameObject ConListPrefab;
         public GameObject ConList;
-        private GameObject _conListItem;
+
+        private Dictionary<ulong, GameObject> conListItems = new Dictionary<ulong, GameObject>();
 
         private void Start()
         {
             NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnect;
             NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnect;
-        
+
             NetworkManager.Singleton.NetworkConfig.ConnectionApproval = true;
             NetworkManager.Singleton.ConnectionApprovalCallback += ApprovalCheck;
         }
@@ -35,7 +38,7 @@ namespace MenuScripts
                     // to set up connection (is local machine by default)
                     // NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = "127.0.0.1";
                     // NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectPort = 7777;
-                
+
                     NetworkManager.Singleton.NetworkConfig.ConnectionData =
                         System.Text.Encoding.Default.GetBytes("password:" + MainMenu.EnteredPass);
                     NetworkManager.Singleton.StartClient();
@@ -48,12 +51,12 @@ namespace MenuScripts
         {
             //parsing connectionData
             var cDataArray = System.Text.Encoding.Default.GetString(connectionData)
-                .Split(new char[]{';'}, StringSplitOptions.RemoveEmptyEntries)      // splits lines
-                .Select(a => a.Split(':'));                               // splits var name from var        
+                .Split(new char[] {';'}, StringSplitOptions.RemoveEmptyEntries) // splits lines
+                .Select(a => a.Split(':')); // splits var name from var        
             var approve = true;
             foreach (var operation in cDataArray)
             {
-                switch (operation[0])   
+                switch (operation[0])
                 {
                     case "password":
                         approve &= operation[1] == MainMenu.EnteredPass;
@@ -74,49 +77,60 @@ namespace MenuScripts
             //If approve is true, the connection gets added. If it's false. The client gets disconnected
             callback(false, prefabHash, approve, new Vector3(0, 0, 0), Quaternion.identity);
         }
-    
+
         public void Disconnect()
         {
-            if (IsHost) 
+            if (NetworkManager.Singleton.IsHost)
             {
                 NetworkManager.Singleton.StopHost();
             }
-            else if (IsServer) 
+            else if (NetworkManager.Singleton.IsServer)
             {
                 NetworkManager.Singleton.StopServer();
             }
-            else if (IsClient) 
+            else if (NetworkManager.Singleton.IsClient)
             {
-                _conListItem.GetComponent<NetworkObject>().Despawn(true);
                 NetworkManager.Singleton.StopClient();
             }
-       
-    
+
+
             UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+        }
+
+        [ClientRpc]
+        void SetNetObjectParentClientRpc(ulong netObjId)
+        {
+            
         }
 
         private void OnClientConnect(ulong id)
         {
-            if (IsClient)
+            if (NetworkManager.Singleton.IsClient)
             {
-                _conListItem = Instantiate(ConListPrefab, ConList.transform);
-                _conListItem.GetComponent<NetworkObject>().Spawn();
+                Debug.Log("IsClient");
             }
-            else if (IsHost)
-                return;
-            else if (IsServer)
-                return;
+
+            if (NetworkManager.Singleton.IsHost)
+                Debug.Log("IsHost");
+            if (NetworkManager.Singleton.IsServer)
+            {
+                var go = Instantiate(ConListPrefab, ConList.transform);
+                go.GetComponent<NetworkObject>().SpawnWithOwnership(id);
+                var netId = go.GetComponent<NetworkObject>().NetworkObjectId;
+                // now RPC the clients to change the parent transform
+                Debug.Log("IsServer");
+            }
         }
 
         private void OnClientDisconnect(ulong id)
         {
-            if (IsHost)
-                return;
-            else if (IsServer)
-                return;
-            else if (IsClient)
+            if (NetworkManager.Singleton.IsServer)
+            {
+                conListItems[id].GetComponent<NetworkObject>().Despawn(true);
+                conListItems.Remove(id);
+            }
+            else if (NetworkManager.Singleton.IsClient)
                 UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
         }
-
     }
 }
--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -1,17 +1,33 @@
+using System.Collections.Generic;
 using MLAPI;
 using MLAPI.SceneManagement;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace MenuScripts
 {
     public class MainMenu : NetworkBehaviour
     {
+
         public static string NetType = "host";
         public static string EnteredPass = "";
-        //public static string requiredPass = "";
+
+        public Canvas[] menus;
+        
         // ------------------ MAIN MENU CODE -------------------------
 
+        public void OpenMainMenu()
+        {
+            foreach (var menu in menus)
+            {
+                menu.gameObject.SetActive(false);
+                if (menu.name != "MainMenu") continue;
+                menu.gameObject.SetActive(true);
+            }
+        }
+        
         public void QuitGame()
         {
             Debug.Log("Quit");
@@ -20,34 +36,69 @@ namespace MenuScripts
     
         // ------------------ LOBBY MENU CODE -------------------------
 
-        public void StartLobbyAsHost()
+        public void OpenLobbyMenuAsHost()
         {
             NetType = "host";
+            foreach (var menu in menus)
+            {
+                menu.gameObject.SetActive(false);
+                if (menu.name != "LobbyMenu") continue;
+                menu.gameObject.SetActive(true);
+                foreach (Transform child in menu.transform)
+                {
+                    if (child.gameObject.name == "StartButton")
+                    {
+                        child.gameObject.SetActive(true);
+                    }
+                }
+            }
         }
     
-        public void JoinLobbyAsClient()
+        public void OpenLobbyMenuAsClient()
         {
             NetType = "client";
+            foreach (var menu in menus)
+            {
+                menu.gameObject.SetActive(false);
+                if (menu.name != "LobbyMenu") continue;
+                menu.gameObject.SetActive(true);
+                foreach (Transform child in menu.transform)
+                {
+                    if (child.gameObject.name == "StartButton")
+                    {
+                        child.gameObject.SetActive(false);
+                    }
+                }
+            }
         }
     
+        // ------------------ PLAY MENU CODE -------------------------
+
+        public void OpenPlayMenu()
+        {
+            foreach (var menu in menus)
+            {
+                menu.gameObject.SetActive(false);
+                if (menu.name != "PlayMenu") continue;
+                menu.gameObject.SetActive(true);
+            }
+        }
+
         public void UpdatePassword(string text)
         {
             EnteredPass = text;
         }
-    
-        // ------------------ LOBBY MENU CODE -------------------------
 
-        public void StartAsHost()
-        {
-            NetType = "host";
-            NetworkSceneManager.SwitchScene("Game");
-        }
-    
-        public void StartAsClient()
-        {
-            NetType = "client";
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
-        }
+        // ------------------ SETTINGS MENU CODE -------------------------
 
+        public void OpenSettingsMenu()
+        {
+            foreach (var menu in menus)
+            {
+                menu.gameObject.SetActive(false);
+                if (menu.name != "SettingsMenu") continue;
+                menu.gameObject.SetActive(true);
+            }
+        }
     }
 }
